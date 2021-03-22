f5c5edec57677cb1b02cc087d0580047211251ca
Brennan <brennan.romance@gmail.com>
2021-03-22 11:41:20 -0400

    add knights travails tree node hierarchy logic, searching next

--- exercises/knights_travails/tree.py
+++ exercises/knights_travails/tree.py
@@ -1,8 +1,10 @@
+import pdb
+
 class Node:
     def __init__(self, value):
         self._value = value
         self._parent = None
-        self._children = []
+        self._children = list()
 
     @property
     def value(self):
@@ -18,8 +20,35 @@ class Node:
 
     @parent.setter
     def parent(self, node):
-        pass
-
+        if self.parent:
+            self.parent.remove_child(self)
+        self._parent = node
+        node.children.append(self)
 
     def add_child(self, node):
-        pass
+        if node.parent == self or node in self.children:
+            return None
+        else:
+            node.parent = self
+
+    def remove_child(self, node):
+        self.children.remove(node)
+
+    def __repr__(self):
+        val = None
+        if self.parent:
+            val = self.parent.value
+        return f"<Node {self.value}, children:{len(self.children)}, parent:{val}>"
+
+    
+bre = Node('brennan')
+der = Node('derek')
+ter = Node('terri')
+kei = Node('keith')
+
+# pdb.set_trace()
+ter.add_child(der)
+ter.add_child(bre)
+
+print(bre)
+print(ter)
